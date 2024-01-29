8f3acad1b0c2c6d2e4882a9c6302bd9b69d71ea5
Radwan Rashique P <radwangoinspire916@gmail.com>
2024-01-29 22:29:37 +0530

    partially completed user controller with 7 routeHandlers corresponding to the 7 api calls , with proper error handling using try catch block

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -2,7 +2,89 @@ const UserModel=require('../Model/UserModel')
 const UserTaskModel=require('../Model/TaskModel')
 
 
-module.exports={
+const RegisterUser=async(req,res)=>{
+    try{
+
+    }
+    catch(error){
+        console.error(error,"At RegisterUser")
+        res.status(500).json({message:"Internal server Error",success:false})
+    }
+}
+
+const UserLogin=async(req,res)=>{
+    try{
+
+    }
+    catch(error){
+        console.error(error,"At LoginUser ")
+        res.status(500).json({message:"Internal server Error",success:false})
+    }
+}
+
+const GetTaskData=async(req,res)=>{
+    try{
+
+    }
+    catch(error){
+        console.error(error,"At GetTaskData")
+        res.status(500).json({message:"Internal server Error",success:false})
+    }
+}
+
+const GetUserName=async(req,res)=>{
+    try{
+
+    }
+    catch(error){
+        console.error(error,"At GetUserName")
+        res.status(500).json({message:'Internal server Error',success:false})
+    }
+}
+
+const AddTask=async(req,res)=>{
+
+    try{
+
+    }
+    catch(error){
+        console.error(error," at AddTask")
+        res.status(500).json({message:"Internal server Error",success:false})
+    }
     
+}
+
+const EditTask=async(req,res)=>{
+    try{
+      
+    }
+    catch(error){
+        console.error(error,"At EditTask")
+        res.status(500).json({message:"Internal server Error",success:false})
+    }
+}
+
+const  DeleteTask=async(req,res)=>{
+    try{
+
+    }
+    catch(error){
+        console.error(error,"At DeleteTask")
+        res.status(500).json({message:"Internal server Error",success:false})
+    }
+
+}
+
+module.exports={
+
+    RegisterUser,
+    UserLogin,
+    GetUserName,
+    GetTaskData,
+    AddTask,
+    EditTask,
+    DeleteTask
+
+
 
 }
